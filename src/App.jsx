0579070695a3c7ce2999c7fe841b598da9d700cf
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-06-13 14:23:40 +0900

    コードの修正
    useEffectの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
 import Header from './components/Header'
